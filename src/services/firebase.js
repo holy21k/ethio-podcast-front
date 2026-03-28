bb3d7c2e9df7f0120cfb47a262067abf90e8fde4
holy21k <kidistteshome@72gmail.com>
2026-03-28 16:33:00 +0300

    move firebase config to env vars

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,16 +1,18 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDBQosoOgqEa0LONZhEZUSVJjV1diFMdCk",
-    authDomain: "ethio-podcast-8a2f0.firebaseapp.com",
-    projectId: "ethio-podcast-8a2f0",
-    storageBucket: "ethio-podcast-8a2f0.firebasestorage.app",
-    messagingSenderId: "976795435136",
-    appId: "1:976795435136:web:fa34145b172223cdad6665",
-    measurementId: "G-268RCQ7PBM"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
+export const storage = getStorage(app);
